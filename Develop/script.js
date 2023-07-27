1b51693457b57723322fa8667916e37871da1124
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-07-27 14:01:14 -0400

    updated script to fix bug where the password was being returned with undefined

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,14 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let specialCharacters = ["+", "-", "&", "#", "!", "(", ")", "$", "%", "", "]", "^", "~", "*", "?"]
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let builtArray = []
+let builtArray = [];
 
-// Write password to the #password input
+// function for generating password
 function generatePassword() {
   //Build array based off user selection
   if (confirm("Do you want the password to contain lowercase letters?")) {
@@ -19,18 +20,21 @@ function generatePassword() {
   if (confirm("Do you want the password to contain numbers?")){
     builtArray = [...builtArray, ...numbers]
   }
-  //Defines password
-  let password = "";
+  if (confirm("Do you want the password to contain special characters?")){
+    builtArray - [...builtArray, ...specialCharacters]
+  }
+  
+  password = "";
   //Prompt for password length and implementation of random char. selector
   let passwordLength = prompt("How long do you want the password?")
   for (let i = 0; i < passwordLength; i++) {
-    let randomCharacter = builtArray[Math.floor(password.Length * Math.random())];
+    let randomCharacter = builtArray[Math.floor(builtArray.length * Math.random())];
     password += randomCharacter;
   }
   //Display Password
   return password;
 }
-
+//function that executes when generate button is clicked
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
